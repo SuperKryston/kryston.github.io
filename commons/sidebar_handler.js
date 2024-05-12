6f13cf9b5e96b95fee96d7967dbbb145b0435c8a
SuperKryston <71617458+SuperKryston@users.noreply.github.com>
2024-05-12 10:53:11 -0600

    updated sidebar handler

--- commons/sidebar_handler.js
+++ commons/sidebar_handler.js
@@ -1,10 +1,17 @@
+const sidebar_menu_element = document.getElementById("sidebar");
 
+const sidebar_elements = [
+`<div class="sidebar_button" onclick="window.location = 'https://superkryston.github.io/'"><label>Home</label></div>`
+
+]
+
+sidebar_elements.forEach((item, index) => {
+    sidebar_menu_element.innerHTML = sidebar_menu_element.innerHTML + item;
+    console.log(item, index);
+});
 
 
 const sidebar_menu_button_open = document.getElementById("sidebar_menu_button_open");
-const sidebar_menu_element = document.getElementById("sidebar");
-
-
 
 sidebar_menu_button_open.addEventListener('click', (event)=> { 
   var flyout = 300;
