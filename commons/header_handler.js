abc414449660820173231a2720f6135f2b7551cf
SuperKryston <71617458+SuperKryston@users.noreply.github.com>
2024-05-13 13:37:05 -0600

    Change the layout of how the page looks

--- commons/header_handler.js
+++ commons/header_handler.js
@@ -21,9 +21,9 @@ header_left_elements.forEach((item, index) => {
 const header_right_element = document.getElementById("header_right");
 
 const header_right_elements = [
-`<a href="https://mcpedl.com/user/kryston/"><div style="display: grid; width:80px; height:70px; padding: 3px; border: 2px solid black; border-radius: 10px; justify-items: center;"><img width="40" height="40" src="https://mcpedl.com/favicon.ico" alt="MCPEDL Logo"><label style="color:black; text-decoration:none;">MCPEDL</label></div></a>`,
-`<a href="https://www.youtube.com/channel/UCj2Q1omyKPmY-dzCuJa6Ggg"><div style="display: grid; width:80px; height:70px; padding: 3px; border: 2px solid black; border-radius: 10px; justify-items: center;"><img width="40" height="40" src="https://youtube.com/favicon.ico" alt="Youtube Logo"><label style="color:black; text-decoration:none;">Youtube</label></div></a>`,
-`<a href="https://discord.gg/jB7qcVwhMr"><div style="display: grid; width:80px; height:70px; padding: 3px; border: 2px solid black; border-radius: 10px; justify-items: center;"><img width="40" height="40" src="https://assets-global.website-files.com/5f8dd67f8fdd6f51f0b50904/60ae916347747e71167e21cc_favicon.png" alt="Discord Logo"><label style="color:black; text-decoration:none;">Discord</label></div></a>`
+`<a href="https://mcpedl.com/user/kryston/"><div class="page_layout_header_right_button_element_body"><img height="40" src="https://mcpedl.com/favicon.ico" alt="MCPEDL Logo"></div></a>`,
+`<a href="https://www.youtube.com/channel/UCj2Q1omyKPmY-dzCuJa6Ggg"><div class="page_layout_header_right_button_element_body"><img height="30" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/512px-YouTube_full-color_icon_%282017%29.svg.png?20240107144800" alt="Youtube Logo"></div></a>`,
+`<a href="https://discord.gg/jB7qcVwhMr"><div class="page_layout_header_right_button_element_body"><img height="30" src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a69f118df70ad7828d4_icon_clyde_blurple_RGB.svg" alt="Discord Logo"></div></a>`
 
 ]
 
