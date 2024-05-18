f26f82949c8a2e2b082f8f563b3804824410202b
SuperKryston <71617458+SuperKryston@users.noreply.github.com>
2024-05-18 12:19:46 -0600

    almost completed

--- store/content_handler.js
+++ store/content_handler.js
@@ -1,23 +1,23 @@
 const content_page_element = document.getElementById("body");
 
 content_page_element.innerHTML = `
-<div id="content_page" class="page_content_content_page">
-        <div style="display:flex;">
-          <div class="grid-item_sub1">
-            <img src="{icon_image}" width=75 height=75 class="grid-item_img">
-          <div class="grid-item_sub2">
-            <label class="page_content_title_text">{item.name}</label><br>
-            <label class="grid-item_sub2t2">{item.type}</label><br>
-            <label class="grid-item_sub2t3">{item.version}</label>
+        <div id="content_page" class="page_content_content_page">
+          <div style="display:flex;">
+            <div class="grid-item_sub1">
+              <img id="page_content_icon_image" src="{icon_image}" width=75 height=75 class="grid-item_img">
+            <div class="grid-item_sub2">
+              <label id="page_content_title_text" class="page_content_title_text">{item.name}</label><br>
+              <label id="page_content_title_type" class="grid-item_sub2t2">{item.type}</label><br>
+              <label id="page_content_title_version" class="grid-item_sub2t3">{item.version}</label>
+            </div>
           </div>
         </div>
-      </div>
-      </div>
-      <div id="buttons">
-      </div>
-      <div id="info">
-      </div>
-      <div id="description">
+        <div id="buttons" style="padding-top: 20px;">
+        </div>
+        <div id="info" style="padding-top: 20px;">
+        </div>
+        <div id="description" style="padding-top: 20px;">
+        </div>
       </div>
 `
 
