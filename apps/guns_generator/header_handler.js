b576c1b914769a52a3ed93c939e8575902717d75
SuperKryston <71617458+SuperKryston@users.noreply.github.com>
2024-08-04 18:27:41 -0600

    Will use new handler

---
+++ apps/guns_generator/header_handler.js
@@ -0,0 +1,32 @@
+const header_left_element = document.getElementById("header_left");
+
+const header_left_elements = [
+`<img style="padding-left: 20px;" width="50" height="50" src="https://superkryston.github.io/assets/button/menu.svg" alt="Menu Bar.png" v:shapes="Picture_x0020_0" id="sidebar_menu_button_open">`,
+`<img style="padding-left: 20px;" width="50" height="50" src="https://superkryston.github.io/assets/logo/K_Border2.svg" alt="K-Logo.png" v:shapes="Picture_x0020_0">`,
+`<label style="font-size: 35px; padding-bottom: 7px;">ryston</label>`
+
+]
+
+header_left_elements.forEach((item, index) => {
+    header_left_element.innerHTML = header_left_element.innerHTML + item;
+});
+
+
+
+
+
+
+
+
+const header_right_element = document.getElementById("header_right");
+
+const header_right_elements = [
+`<a href="https://mcpedl.com/user/kryston/"><div class="page_layout_header_right_button_element_body"><img height="40" src="https://mcpedl.com/favicon.ico" alt="MCPEDL Logo"></div></a>`,
+`<a href="https://www.youtube.com/channel/UCj2Q1omyKPmY-dzCuJa6Ggg"><div class="page_layout_header_right_button_element_body"><img height="30" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/512px-YouTube_full-color_icon_%282017%29.svg.png?20240107144800" alt="Youtube Logo"></div></a>`,
+`<a href="https://discord.gg/jB7qcVwhMr"><div class="page_layout_header_right_button_element_body"><img height="30" src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a69f118df70ad7828d4_icon_clyde_blurple_RGB.svg" alt="Discord Logo"></div></a>`
+
+]
+
+header_right_elements.forEach((item, index) => {
+    header_right_element.innerHTML = header_right_element.innerHTML + item;
+});
---
+++ apps/guns_generator/sidebar_handler.js
@@ -0,0 +1,45 @@
+const sidebar_menu_element = document.getElementById("sidebar");
+
+sidebar_menu_element.innerHTML = `
+      <div style="display: flex;align-items: center;align-content: center; padding-top: 20px; background: #797979; padding-bottom: 20px;">
+        <img style="padding-left: 20px;" width="50" height="50" src="https://superkryston.github.io/assets/button/arrow_left_dark.svg" alt="Menu Bar.png" v:shapes="Picture_x0020_0" id="sidebar_menu_button_close">
+        <img style="padding-left: 20px;" width="50" height="50" src="https://superkryston.github.io/assets/logo/K_Border2.svg" alt="K-Logo.png" v:shapes="Picture_x0020_0">
+        <label style="font-size: 35px; padding-bottom: 7px;">ryston</label>
+      </div>
+`
+
+const sidebar_elements = [
+`<div class="sidebar_button" onclick="window.location = 'https://superkryston.github.io/'"><label>Home</label></div>`,
+`<div class="sidebar_button" onclick="window.location = 'https://www.appsheet.com/start/d8eb3668-6bc6-4736-b86c-6d91fdd35f58'"><label>Appsheet Store</label></div>`,
+`<div class="sidebar_button" onclick="window.location = 'https://superkryston.github.io/store'"><label>Contents</label></div>`,
+`<div class="sidebar_button" onclick="window.location = 'https://superkryston.github.io/donate'"><label>Donate</label></div>`
+
+]
+
+sidebar_elements.forEach((item, index) => {
+    sidebar_menu_element.innerHTML = sidebar_menu_element.innerHTML + item;
+    //console.log(item, index);
+});
+ 
+
+const sidebar_menu_button_open = document.getElementById("sidebar_menu_button_open");
+
+sidebar_menu_button_open.addEventListener('click', (event)=> { 
+  var flyout = 300;
+  sidebar_menu_element.style.width = flyout;
+
+
+  sidebar_menu_element.style.left = 0;
+ // for (let i = 0; i < flyout; i++) {
+ //     sidebar_menu_element.style.width = i;
+ // }
+})
+
+sidebar_menu_button_close.addEventListener('click', (event)=> { 
+  sidebar_menu_element.style.left = -300;
+})
+
+sidebar_menu_button_open.addEventListener('contextmenu', (event)=> { 
+    event.preventDefault();
+    window.location = `https://www.youtube.com/watch?v=dQw4w9WgXcQ`;
+})
